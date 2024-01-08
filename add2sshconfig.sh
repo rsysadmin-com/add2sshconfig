6122edfc6b215c5a228e9559d96ed9e2791e01ad
Martin Mielke <martinm@rsysadmin.com>
2024-01-08 15:45:51 +0100

    Accept jump host as 3rd arg.

--- add2sshconfig.sh
+++ add2sshconfig.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# v0.1 by martinm@rsysadmin.com
+# v0.2 by martinm@rsysadmin.com
 #
 
 ssh_config=~/.ssh/config
 host=$1
 ip=$2
+jumphost=$3
 
 function usage() {
 cat << EOT
 $(basename $0) hostname ip.add.re.ss
-  e.g.: $(basename $0) foobar 10.10.10.10
+  e.g.: $(basename $0) foobar 10.10.10.10 10.10.10.100
 EOT
    exit 1
 }
 
-[[ $# -lt 2 ]] && usage
+[[ $# -lt 3 ]] && usage
 
 egrep $host $ssh_config > /dev/null
 if [ $? -eq 0 ]
@@ -28,12 +29,12 @@ else
     echo "Host $host
       User $USER
       Hostname $ip
-      ProxyCommand ssh $USER@10.0.104.10 -W %h:%p
+      ProxyCommand ssh $USER@${jump_host} -W %h:%p
 
 Host $ip
       User $USER
       Hostname $ip
-      ProxyCommand ssh $USER@10.0.104.10 -W %h:%p
+      ProxyCommand ssh $USER@${jump_host} -W %h:%p
 " | tee -a $ssh_config
 fi
 
